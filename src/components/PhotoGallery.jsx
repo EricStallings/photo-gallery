2a68f1a786c7f18f2c72007dedc237accc68d0b7
Eric Stallings <stallings.eric@gmail.com>
2019-12-24 16:43:47 -0800

    Added initial Photo Gallery Component.

---
+++ src/components/PhotoGallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+class PhotoGallery extends Component{
+  constructor(props){
+    super(props)
+    return(
+      <h1>Hello!</h1>
+    )
+  }
+}
+
+export default PhotoGallery;
